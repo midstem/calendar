b0ee3ac0778102806f5dec97e073eab5337d64e0
Maks Tkachyk <tkachykmakc@gmail.com>
2023-06-06 15:52:55 +0300

    Replace MUI icons

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,13 @@
+import { CSSProperties } from 'react'
+
+import { IconSizes } from './constants'
+
 export type ValueOF<T extends string> = `${T}`
 
 export type ButtonStylesT = 'primary' | 'secondary'
+
+export type IconsT = {
+  sx?: CSSProperties
+  color?: string
+  size?: ValueOF<IconSizes>
+}
