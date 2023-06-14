0b55a47b844780457c59452243bf94cb1e45c3e3
Oleksandr <didyshen.oleksandr@gmail.com>
2023-06-14 12:47:15 +0300

    Remove fragments from svg components

--- src/components/ChevronDown/index.tsx
+++ src/components/ChevronDown/index.tsx
@@ -8,21 +8,19 @@ const ChevronDown = ({
   size = IconSizes.xs,
 }: IconsT): JSX.Element => {
   return (
-    <>
-      <svg
-        style={sx}
-        width={size}
-        height={size}
-        focusable="false"
-        aria-hidden="true"
-        viewBox="0 0 11 6"
-      >
-        <path
-          fill={color}
-          d="M0.117961 0.682839C-0.134029 0.430859 0.0444411 0 0.400801 0H9.66939C10.0258 0 10.2043 0.430859 9.95229 0.682839L5.31799 5.31719C5.16179 5.47339 4.90849 5.47339 4.75229 5.31719L0.117961 0.682839Z"
-        ></path>
-      </svg>
-    </>
+    <svg
+      style={sx}
+      width={size}
+      height={size}
+      focusable="false"
+      aria-hidden="true"
+      viewBox="0 0 11 6"
+    >
+      <path
+        fill={color}
+        d="M0.117961 0.682839C-0.134029 0.430859 0.0444411 0 0.400801 0H9.66939C10.0258 0 10.2043 0.430859 9.95229 0.682839L5.31799 5.31719C5.16179 5.47339 4.90849 5.47339 4.75229 5.31719L0.117961 0.682839Z"
+      ></path>
+    </svg>
   )
 }
 
--- src/components/LeftArrow/index.tsx
+++ src/components/LeftArrow/index.tsx
@@ -7,21 +7,19 @@ const LeftArrow = ({
   size = IconSizes.md,
 }: IconsT): JSX.Element => {
   return (
-    <>
-      <svg
-        style={sx}
-        width={size}
-        height={size}
-        focusable="false"
-        aria-hidden="true"
-        viewBox="0 0 24 24"
-      >
-        <path
-          fill={color}
-          d="M15.41 16.59 10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"
-        ></path>
-      </svg>
-    </>
+    <svg
+      style={sx}
+      width={size}
+      height={size}
+      focusable="false"
+      aria-hidden="true"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill={color}
+        d="M15.41 16.59 10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"
+      ></path>
+    </svg>
   )
 }
 
