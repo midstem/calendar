1d48cd2489ee8e43b69a5427d03051dd7c8bf06f
Oleksandr Didyshen <102871422+alex-dishen@users.noreply.github.com>
2023-06-19 10:49:28 +0300

    Setup project for publishing to npm (#58)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,17 @@
 import ReactDOM from 'react-dom/client'
+import './styles/index.css'
+import './styles/font.css'
 
-import App from './App'
+import Calendar from './features/Calendar'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />,
+  <div className="paper">
+    <Calendar
+      mode="Week"
+      config={[{ maxWidth: 850, mode: 'Week' }]}
+      onClickCell={console.log}
+      onClickEvent={console.log}
+      selectedEvent="98f6c30c-a95e-4794-b2ee-5c0143cbe0b5"
+    />
+  </div>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,22 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+export default defineConfig(() => ({
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 8080,
+  },
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve('src', 'index.tsx'),
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react'],
+    },
+  },
+}))
