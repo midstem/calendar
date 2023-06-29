7add66a16b4ca687d2e95e1c026f3a451bdb7734
Igor Kozlovsky <65256966+IgorKozlovsky@users.noreply.github.com>
2023-06-29 15:13:12 +0300

    fixed scroll, styling selected day, add selected day to month view, f… (#128)
    * fixed scroll, styling selected day, add selected day to month view, fixed cell time shrink
    
    * time cells dynamic width

--- src/features/MonthView/types.ts
+++ src/features/MonthView/types.ts
@@ -4,4 +4,5 @@ import { MonthCellType, UserEvents } from '../../types'
 export type MonthViewProps = UserEvents<MonthCellType> & {
   renderRows: Cell[]
   selectDateHandler: (date: Date) => void
+  selectedDate: Date
 }
