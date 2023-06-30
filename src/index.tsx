c9702fb7467f70ef25767d1f5f1b820ec8019b37
Igor Kozlovsky <65256966+IgorKozlovsky@users.noreply.github.com>
2023-06-30 15:17:30 +0300

    136 fix header grid (#141)
    * fix header grid
    
    * fix header grid
    
    * Update styles.css
    
    * Improve grids
    
    ---------
    
    Co-authored-by: Oleksandr <didyshen.oleksandr@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,21 @@
+// import './styles/index.css'
+// import Chronous from './Chronous'
+
+// export default Chronous
+
+import ReactDOM from 'react-dom/client'
 import './styles/index.css'
+
 import Chronous from './Chronous'
 
-export default Chronous
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <div className="paper">
+    <Chronous
+      mode="Week"
+      config={[{ maxWidth: 850, mode: 'Week' }]}
+      onClickCell={console.log}
+      onClickEvent={console.log}
+      selectedEvent="98f6c30c-a95e-4794-b2ee-5c0143cbe0b5"
+    />
+  </div>,
+)
